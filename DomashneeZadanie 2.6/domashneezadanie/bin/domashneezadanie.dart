d60cfc335f28b9d77577221572438d921e3c53cb
BliznGg <116829276+BliznGg@users.noreply.github.com>
2022-11-08 20:05:31 +0200

    Add files via upload

---
+++ DomashneeZadanie 2.6/domashneezadanie/bin/domashneezadanie.dart
@@ -0,0 +1,5 @@
+import 'package:domashneezadanie/domashneezadanie.dart' as domashneezadanie;
+
+void main(List<String> arguments) {
+  print('Hello world: ${domashneezadanie.calculate()}!');
+}
---
+++ DomashneeZadanie 2.6/domashneezadanie/lib/nokANDnod.dart
@@ -0,0 +1,15 @@
+import 'dart:collection';
+import 'dart:math';
+
+void main() {
+  class DelimetersCalculator {
+    int = a 
+    int = b 
+    ia, ib =a, b 
+    while ia !=ib: 
+        if ia > ib: ia =ia-ib 
+        else: ib = ib - ia 
+print("НОД =", ia)
+print("НОК"=, a*b // ia)
+  }
+}
